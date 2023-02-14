dba16e71bff6d6903e143e8260b6cffb05b59986
JoaoManoelFontes <jmanufontesc@gmail.com>
2023-02-14 01:57:36 -0300

    add django models

--- Django/bookShop/models.py
+++ Django/bookShop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
 
 # Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    synopsis = models.TextField(null=True, blank=True)
+    author_name = models.CharField(max_length=255)
+    publishing_company_name = models.CharField(max_length=255)
+    release_year = models.DateField()
+    price = models.FloatField()
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Sale(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    seller = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    book_title = models.CharField(max_length=255)
+    client_name = models.CharField(max_length=255, null=True, blank=True)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- Django/bookShop/views.py
+++ Django/bookShop/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 
 
+# Rota inicial - mostragem de livros disponíveis
 def home(request):
     return HttpResponse("oi")
+
+
+# Roda de venda - vender um livro para o cliente
+
+# Rota de cadastro de livros
+
+# Rota de edição de livros
+
+# Rota de histórico de vendas
